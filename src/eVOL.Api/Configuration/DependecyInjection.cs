3c04ac16292b1705edb1ba2018c122b631b5c3b1
G G <UNION SELECT username FROM users WHERE id = 1 -->
2025-11-19 14:46:50 +0200

    Seperated services from program.cs to DI Folder using IServiceCollection

---
+++ src/eVOL.Api/Configuration/DependecyInjection.cs
@@ -0,0 +1,209 @@
+﻿using eVOL.Application.Mappings;
+using eVOL.Application.Messaging.Interfaces;
+using eVOL.Application.ServicesInterfaces;
+using eVOL.Application.UseCases.AdminCases;
+using eVOL.Application.UseCases.ChatGroupCases;
+using eVOL.Application.UseCases.SupportTicketCases;
+using eVOL.Application.UseCases.UCInterfaces.IAdminCases;
+using eVOL.Application.UseCases.UCInterfaces.IChatGroupCases;
+using eVOL.Application.UseCases.UCInterfaces.ISupportTicketCases;
+using eVOL.Application.UseCases.UCInterfaces.IUserCases;
+using eVOL.Application.UseCases.UserCases;
+using eVOL.Domain.RepositoriesInteraces;
+using eVOL.Infrastructure.Data;
+using eVOL.Infrastructure.Persistence;
+using eVOL.Infrastructure.Repositories;
+using eVOL.Infrastructure.Services;
+using Mapster;
+using MapsterMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using StackExchange.Redis;
+using System.Text;
+
+namespace eVOL.API.Configuration
+{
+    public static class DependecyInjection
+    {
+
+        public static IServiceCollection AddCaching(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            var redisConnectionString = configuration["CacheSettings:RedisConnection"];
+            var options = ConfigurationOptions.Parse(redisConnectionString);
+            options.AbortOnConnectFail = false;
+
+            services.AddSingleton<IConnectionMultiplexer>(
+                ConnectionMultiplexer.Connect(options)
+            );
+
+            services.AddSingleton<IConnectionMultiplexer>(
+                ConnectionMultiplexer.Connect(options)
+            );
+
+            var redisConnection = configuration["CacheSettings:RedisConnection"];
+            services.AddSignalR().AddStackExchangeRedis(redisConnection, opts =>
+            {
+                opts.Configuration.ChannelPrefix = "evol.signalr";
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddDatabases(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySql(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    Microsoft.EntityFrameworkCore.ServerVersion.AutoDetect(
+                        configuration.GetConnectionString("DefaultConnection")
+                    )
+                )
+            );
+
+            services.AddSingleton<MongoDbContext>(sp =>
+            {
+                var config = configuration;
+                var connectionString = config.GetConnectionString("MongoDB");
+                return new MongoDbContext(connectionString, "eVOLChat");
+            });
+
+            services.AddScoped<IClientSessionHandle>(sp =>
+            {
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.StartSession();
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuthenticationAndAuthorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                    };
+            });
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddCorsService(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .WithOrigins("http://localhost:5001")
+                          .AllowCredentials();
+                });
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddMapper(this IServiceCollection services)
+        {
+            var config = new TypeAdapterConfig();
+            config.Scan(typeof(UserMappings).Assembly);
+
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, ServiceMapper>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddScopedUseCases(this IServiceCollection services)
+        {
+            // Services
+
+            services.AddScoped<IJwtService, JwtService>();
+            services.AddScoped<ICacheService, CacheService>();
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+            services.AddScoped<IAesService, AesService>();
+
+            // Messaging
+
+            services.AddScoped<IRabbitMqPublisher, RabbitMqPublisher>();
+
+            // Admin Use Cases
+
+            services.AddScoped<IAdminDeleteUserUseCase, AdminDeleteUserUseCase>();
+            services.AddScoped<IAdminGetUserUseCase, AdminGetUserUseCase>();
+
+            // User Use Cases
+
+            services.AddScoped<IDeleteUserUseCase, DeleteUserUseCase>();
+            services.AddScoped<IGetUserUseCase, GetUserUseCase>();
+            services.AddScoped<ILoginUserUseCase, LoginUserUseCase>();
+            services.AddScoped<IRefreshTokenUseCase, RefreshTokenUseCase>();
+            services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+            services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
+
+            // Chat Group Use Cases
+
+            services.AddScoped<IAddUserToChatGroupUseCase, AddUserToChatGroupUseCase>();
+            services.AddScoped<ICreateChatGroupUseCase, CreateChatGroupUseCase>();
+            services.AddScoped<IDeleteChatGroupUseCase, DeleteChatGroupUseCase>();
+            services.AddScoped<IGetChatGroupByIdUseCase, GetChatGroupByIdUseCase>();
+            services.AddScoped<ILeaveChatGroupUseCase, LeaveChatGroupUseCase>();
+            services.AddScoped<IRemoveUserFromChatGroupUseCase, RemoveUserFromChatGroupUseCase>();
+            services.AddScoped<ISendChatGroupMessageUseCase, SendChatGroupMessageUseCase>();
+            services.AddScoped<ITransferOwnershipOfChatGroupUseCase, TransferOwnershipOfChatGroupUseCase>();
+
+            // Support Ticket Use Cases
+
+            services.AddScoped<IClaimSupportTicketUseCase, ClaimSupportTicketUseCase>();
+            services.AddScoped<ICreateSupportTicketUseCase, CreateSupportTicketUseCase>();
+            services.AddScoped<IDeleteSupportTicketUseCase, DeleteSupportTicketUseCase>();
+            services.AddScoped<IGetSupportTicketByIdUseCase, GetSupportTicketByIdUseCase>();
+            services.AddScoped<ISendSupportTicketMessageUseCase, SendSupportTicketMessageUseCase>();
+            services.AddScoped<IUnClaimSupportTicketUseCase, UnClaimSupportTicketUseCase>();
+
+
+            // Repositories
+
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IChatGroupRepository, ChatGroupRepository>();
+            services.AddScoped<ISupportTicketRepository, SupportTicketRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+
+            // Unit's Of Work
+
+            services.AddScoped<IMySqlUnitOfWork, MySqlUnitOfWork>();
+            services.AddScoped<IMongoUnitOfWork, MongoUnitOfWork>();
+
+
+
+            return services;
+        }
+    }
+
+    
+}
--- src/eVOL.Api/Program.cs
+++ src/eVOL.Api/Program.cs
@@ -1,30 +1,7 @@
-using Mapster;
-using MapsterMapper;
 using eVOL.API.Hubs;
-using eVOL.Application.Mappings;
-using eVOL.Application.Messaging.Interfaces;
-using eVOL.Application.ServicesInterfaces;
-using eVOL.Application.UseCases.AdminCases;
-using eVOL.Application.UseCases.ChatGroupCases;
-using eVOL.Application.UseCases.SupportTicketCases;
-using eVOL.Application.UseCases.UCInterfaces.IAdminCases;
-using eVOL.Application.UseCases.UCInterfaces.IChatGroupCases;
-using eVOL.Application.UseCases.UCInterfaces.ISupportTicketCases;
-using eVOL.Application.UseCases.UCInterfaces.IUserCases;
-using eVOL.Application.UseCases.UserCases;
-using eVOL.Domain.RepositoriesInteraces;
-using eVOL.Infrastructure.Data;
-using eVOL.Infrastructure.Persistence;
-using eVOL.Infrastructure.Repositories;
-using eVOL.Infrastructure.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using MongoDB.Driver;
 using Serilog;
-using StackExchange.Redis;
-using System;
-using System.Text;
+using eVOL.API.Configuration;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
@@ -42,156 +19,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        Microsoft.EntityFrameworkCore.ServerVersion.AutoDetect(
-            builder.Configuration.GetConnectionString("DefaultConnection")
-        )
-    )
-);
-
-builder.Services.AddSingleton<MongoDbContext>(sp =>
-{
-    var config = builder.Configuration;
-    var connectionString = config.GetConnectionString("MongoDB");
-    return new MongoDbContext(connectionString, "eVOLChat");
-});
-
-builder.Services.AddScoped<IClientSessionHandle>(sp =>
-{
-    var client = sp.GetRequiredService<IMongoClient>();
-    return client.StartSession(); 
-});
-
-
-
-
-var redisConnectionString = builder.Configuration["CacheSettings:RedisConnection"];
-var options = ConfigurationOptions.Parse(redisConnectionString);
-options.AbortOnConnectFail = false;
-
-builder.Services.AddSingleton<IConnectionMultiplexer>(
-    ConnectionMultiplexer.Connect(options)
-);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-{
-options.TokenValidationParameters = new TokenValidationParameters
-{
-ValidateIssuer = true,
-ValidateAudience = true,
-ValidateLifetime = true,
-ValidateIssuerSigningKey = true,
-ValidIssuer = builder.Configuration["Jwt:Issuer"],
-ValidAudience = builder.Configuration["Jwt:Audience"],
-IssuerSigningKey = new SymmetricSecurityKey(
-        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-};
-});
-
-
-builder.Services.AddEndpointsApiExplorer();
-
-var redisConnection = builder.Configuration["CacheSettings:RedisConnection"];
-builder.Services.AddSignalR().AddStackExchangeRedis(redisConnection, opts =>
-{
-    opts.Configuration.ChannelPrefix = "evol.signalr";
-});
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod()
-              .AllowAnyHeader()  
-              .WithOrigins("http://localhost:5001")
-              .AllowCredentials();
-    });
-});
-
-var config = new TypeAdapterConfig();
-config.Scan(typeof(UserMappings).Assembly);
-
-builder.Services.AddSingleton(config);
-builder.Services.AddScoped<IMapper, ServiceMapper>();
-
-// Services
-
-builder.Services.AddScoped<IJwtService, JwtService>();
-builder.Services.AddScoped<ICacheService, CacheService>();
-builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
-builder.Services.AddScoped<IAesService, AesService>();
-
-// Messaging
-
-builder.Services.AddScoped<IRabbitMqPublisher, RabbitMqPublisher>();
-
-// Admin Use Cases
-
-builder.Services.AddScoped<IAdminDeleteUserUseCase, AdminDeleteUserUseCase>();
-builder.Services.AddScoped<IAdminGetUserUseCase, AdminGetUserUseCase>();
-
-// User Use Cases
-
-builder.Services.AddScoped<IDeleteUserUseCase, DeleteUserUseCase>();
-builder.Services.AddScoped<IGetUserUseCase, GetUserUseCase>();
-builder.Services.AddScoped<ILoginUserUseCase, LoginUserUseCase>();
-builder.Services.AddScoped<IRefreshTokenUseCase, RefreshTokenUseCase>();
-builder.Services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
-builder.Services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
-
-// Chat Group Use Cases
-
-builder.Services.AddScoped<IAddUserToChatGroupUseCase, AddUserToChatGroupUseCase>();
-builder.Services.AddScoped<ICreateChatGroupUseCase, CreateChatGroupUseCase>();
-builder.Services.AddScoped<IDeleteChatGroupUseCase, DeleteChatGroupUseCase>();
-builder.Services.AddScoped<IGetChatGroupByIdUseCase, GetChatGroupByIdUseCase>();
-builder.Services.AddScoped<ILeaveChatGroupUseCase, LeaveChatGroupUseCase>();
-builder.Services.AddScoped<IRemoveUserFromChatGroupUseCase, RemoveUserFromChatGroupUseCase>();
-builder.Services.AddScoped<ISendChatGroupMessageUseCase, SendChatGroupMessageUseCase>();
-builder.Services.AddScoped<ITransferOwnershipOfChatGroupUseCase, TransferOwnershipOfChatGroupUseCase>();
-
-// Support Ticket Use Cases
-
-builder.Services.AddScoped<IClaimSupportTicketUseCase, ClaimSupportTicketUseCase>();
-builder.Services.AddScoped<ICreateSupportTicketUseCase, CreateSupportTicketUseCase>();
-builder.Services.AddScoped<IDeleteSupportTicketUseCase, DeleteSupportTicketUseCase>();
-builder.Services.AddScoped<IGetSupportTicketByIdUseCase, GetSupportTicketByIdUseCase>();
-builder.Services.AddScoped<ISendSupportTicketMessageUseCase, SendSupportTicketMessageUseCase>();
-builder.Services.AddScoped<IUnClaimSupportTicketUseCase, UnClaimSupportTicketUseCase>();
-
-
-// Repositories
-
-builder.Services.AddScoped<IAdminRepository, AdminRepository>();
-builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IChatGroupRepository, ChatGroupRepository>();
-builder.Services.AddScoped<ISupportTicketRepository, SupportTicketRepository>();
-builder.Services.AddScoped<IMessageRepository, MessageRepository>();
-
-// Unit's Of Work
-
-builder.Services.AddScoped<IMySqlUnitOfWork, MySqlUnitOfWork>();
-builder.Services.AddScoped<IMongoUnitOfWork, MongoUnitOfWork>();
+builder.Services
+    .AddCaching(builder.Configuration)
+    .AddDatabases(builder.Configuration)
+    .AddPresentation()
+    .AddAuthenticationAndAuthorization(builder.Configuration)
+    .AddCorsService()
+    .AddMapper()
+    .AddScopedUseCases();
 
 var app = builder.Build();
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
 app.UseRouting();
 
 app.UseCors("CorsPolicy");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
